c3866b88e723817cc868ce4a28495a7d43fa779b
Jiaying330 <jiaying.hou330@gmail.com>
2024-09-13 17:44:18 -0700

    why section

--- nasdaq-private-market/src/components/Assets/Sections/Sections.tsx
+++ nasdaq-private-market/src/components/Assets/Sections/Sections.tsx
@@ -1,12 +1,14 @@
 import ProductSection from "./ProductSection/ProductSection";
 import ServiceSection from "./ServiceSection/ServiceSection";
 import "./Section.scss";
+import WhySection from "./WhySection/WhySection";
 
 export default function Sections() {
   return (
     <>
       <ProductSection />
       <ServiceSection />
+      <WhySection />
     </>
   );
 }
---
+++ nasdaq-private-market/src/components/Assets/Sections/WhySection/InfoGrid/InfoGrid.tsx
@@ -0,0 +1,23 @@
+import "./InfoGrid.scss";
+import { SvgIconComponent } from "@mui/icons-material";
+
+type propType = {
+  icon: SvgIconComponent;
+  heading: string;
+  infos: string[];
+};
+export default function InfoGrid(prop: propType) {
+  return (
+    <div className="info">
+      <div className="info__icon">
+        <prop.icon />
+      </div>
+      <h4>{prop.heading}</h4>
+      <ul className="info__content">
+        {prop.infos.map((info) => (
+          <li>{info}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
